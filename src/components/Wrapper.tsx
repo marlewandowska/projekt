eae9fa338fde88b35a196f0de55202dd91ae6be5
mlewandowska <marti.lewan@gmail.com>
2023-05-18 09:32:36 +0200

    dzialajace okienka login register

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,7 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Wrapper = () => {
-  const [trueOrFalse, setTrueOrFalse] = useState(true);
+  const [trueOrFalse, setTrueOrFalse] = useState(
+    localStorage.getItem("loggedIn") === "true"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("loggedIn", String(trueOrFalse));
+  }, [trueOrFalse]);
+
+  useEffect(() => {
+    if (trueOrFalse) {
+      document.querySelector('.wrapper')?.classList.remove('increased-height');
+    } else {
+      document.querySelector('.wrapper')?.classList.add('increased-height');
+    }
+  }, [trueOrFalse]);
 
   function zmienNaFalse() {
     setTrueOrFalse(false);
@@ -11,93 +25,85 @@ const Wrapper = () => {
     setTrueOrFalse(true);
   }
 
-  if (trueOrFalse === true) {
-    return (
-      <div className="wrapper">
-        <div className="form-box login">
-          <h2>Login</h2>
-          <form action="#">
-            <div className="input-box">
-              <span className="icon"></span>
-              <input type="email" required />
-              <label>Email</label>
-            </div>
-            <div className="input-box">
-              <span className="icon"></span>
-              <input type="password" required />
-              <label>Password</label>
-            </div>
-            <div className="remember-forgot">
-              <label>
-                <input type="checkbox" />Remember me
-              </label>
-              <a href="#">Forgot Password?</a>
-            </div>
-            <button type="submit" className="btn">
-              Login
-            </button>
-            <div className="login-register">
-              <p>
-                Don't have an account?{" "}
-                <button className="register-link" onClick={zmienNaFalse}>
-                  Register
-                </button>
-              </p>
-            </div>
-          </form>
-        </div>
+  return (
+    <div className={`wrapper${trueOrFalse ? "" : " increased-height"}`}>
+      <div className="form-box login">
+        <h2>{trueOrFalse ? "Login" : "Register"}</h2>
+        <form action="#">
+          {trueOrFalse ? (
+            <>
+              <div className="input-box">
+                <span className="icon"></span>
+                <input type="email" required />
+                <label>Email</label>
+              </div>
+              <div className="input-box">
+                <span className="icon"></span>
+                <input type="password" required />
+                <label>Password</label>
+              </div>
+              <div className="remember-forgot">
+                <label>
+                  <input type="checkbox" />Remember me
+                </label>
+                <a href="#">Forgot Password?</a>
+              </div>
+              <button type="submit" className="btn">
+                Login
+              </button>
+              <div className="login-register">
+                <p>
+                  Don't have an account?{" "}
+                  <button className="register-link" onClick={zmienNaFalse}>
+                    Register
+                  </button>
+                </p>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="input-box">
+                <span className="icon"></span>
+                <input type="email" required />
+                <label>Email</label>
+              </div>
+              <div className="input-box">
+                <span className="icon"></span>
+                <input type="password" required />
+                <label>Username</label>
+              </div>
+              <div className="input-box">
+                <span className="icon"></span>
+                <input type="password" required />
+                <label>Password</label>
+              </div>
+              <div className="input-box">
+                <span className="icon"></span>
+                <input type="password" required />
+                <label>Repeat Password</label>
+              </div>
+              <div className="remember-forgot">
+                <label>
+                  <input type="checkbox" />Accept
+                </label>
+              </div>
+              <button type="submit" className="btn">
+                Register
+              </button>
+              <div className="login-register">
+                <p>
+                  Already have an account?{" "}
+                  <button className="login-link" onClick={zmienNaTrue}>
+                    Login
+                  </button>
+                </p>
+              </div>
+            </>
+          )}
+        </form>
       </div>
-    );
-  } else if (trueOrFalse === false) {
-    return (
-      <div className="wrapper">
-        <div className="form-box login">
-          <h2>Register</h2>
-          <form action="#">
-            <div className="input-box">
-              <span className="icon"></span>
-              <input type="email" required />
-              <label>Email</label>
-            </div>
-            <div className="input-box">
-              <span className="icon"></span>
-              <input type="password" required />
-              <label>Username</label>
-            </div>
-            <div className="input-box">
-              <span className="icon"></span>
-              <input type="password" required />
-              <label>Password</label>
-            </div>
-            <div className="input-box">
-              <span className="icon"></span>
-              <input type="password" required />
-              <label>Repeat Password</label>
-            </div>
-
-            <div className="remember-forgot">
-              <label>
-                <input type="checkbox" />Accept
-              </label>
-            </div>
-            <button type="submit" className="btn">
-              Register
-            </button>
-            <div className="login-register">
-              <p>
-                Already have an account?{" "}
-                <button className="login-link" onClick={zmienNaTrue}>
-                  Login
-                </button>
-              </p>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
-
-  return null; // Dodana linia zwracająca null dla innych przypadków
+    </div>
+  );
 };
 
 export { Wrapper };
